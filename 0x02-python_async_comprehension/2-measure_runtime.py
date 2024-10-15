d315dda22d0b31e15bb80c0fac6338df847b00b5
Ahmed Muawia <amuawia666@gmail.com>
2024-10-15 15:49:42 +0300

    Returned the runtime directly

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -22,6 +22,4 @@ async def measure_runtime() -> float:
             async_comprehension(),
             )
 
-    total_time = time.perf_counter() - start_time
-
-    return total_time
+    return time.perf_counter() - start_time
