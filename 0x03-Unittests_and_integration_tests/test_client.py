bd5c8670fdfdc7fd9ecbc195aff7286e9cff4948
Ahmed Muawia <amuawia666@gmail.com>
2024-11-05 03:09:49 +0200

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,7 +9,7 @@ from parameterized import parameterized
 from utils import get_json
 from client import GithubOrgClient
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import requests
 
 
@@ -18,8 +18,8 @@ class TestGithubOrgClient(unittest.TestCase):
     Tests client.GithubOrgClient
     """
     @parameterized.expand([
-        ("google",{'login': "google"}),
-        ("abc",{'login': "abc"})
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"})
         ])
     @patch('client.get_json')
     def test_org(self, org_name, expected, mock_get_json):
@@ -31,5 +31,20 @@ class TestGithubOrgClient(unittest.TestCase):
 
         result = client.org
 
-        mock_get_json.assert_called_once_with(client.ORG_URL.format(org=org_name))
+        mock_get_json.assert_called_once_with(
+                client.ORG_URL.format(org=org_name))
         self.assertEqual(result, expected)
+
+    def test_public_repos_url(self):
+        """
+        Mockes a readonly property with mock
+        """
+        with patch(
+                'client.GithubOrgClient.org',
+                new_callable=PropertyMock) as mock_public_repos_url:
+            payload = {"repos_url": "Any_paylod"}
+            mock_public_repos_url.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+
+            self.assertEqual(result, payload['repos_url'])
